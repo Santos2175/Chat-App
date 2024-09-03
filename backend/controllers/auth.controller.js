c6149f78038155fdadc0d1b543fe4448761a6c1e
Santos2175 <santoshgurung949@gmail.com>
2024-09-03 18:31:17 +0545

    signup backend added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,46 @@
+import User from '../models/user.model.js';
+
 //handles signup
-export const signup = (req, res) => {
-  res.send('SignUp Route');
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ msg: 'Passwords do not match' });
+    }
+    const user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ msg: 'User with this username already exists' });
+    }
+
+    //Hash password
+
+    //https://avatar-placeholder.iran.liara.run/
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === 'male' ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profilePic,
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
 };
 
 //handles login
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
-const app = express();
-
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 9000;
 
+//middlewares
+app.use(express.json());
+
+//routes
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
