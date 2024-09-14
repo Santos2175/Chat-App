63195013b7004fe3b89818fdbe887cd8bec50934
Santos2175 <santoshgurung949@gmail.com>
2024-09-14 20:18:02 +0545

    socket io url change for deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:9000', {
+      const socket = io('https://chat-app-qo06.onrender.com/', {
         query: {
           userId: authUser._id,
         },
